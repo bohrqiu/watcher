71b692e38ef4691342294c000398d5e7035fa1b4
邱占波 <bohr.qiu@gmail.com>
2015-04-29 11:48:35 +0800

    remove yiji dependencies

--- src/test/java/com/yiji/framework/watcher/WatherTomcatBootStrap.java
+++ src/test/java/com/yiji/framework/watcher/WatherTomcatBootStrap.java
@@ -5,7 +5,7 @@ package com.yiji.framework.watcher;
  * @author Bohr.Qiu <qzhanbo@yiji.com>
  * 
  */
-public class CSTomcatBootStrap {
+public class WatherTomcatBootStrap {
 	public static void main(final String[] args) {
 		new BootstrapHelper(11111, false, "local").start();
 	}
