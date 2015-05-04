f2aabfe15262caa5782e021a479fa009e5fef295
邱占波 <bohr.qiu@gmail.com>
2015-05-04 16:13:34 +0800

    modify test

--- src/test/groovy/MonitorServiceSpec.groovy
+++ src/test/groovy/MonitorServiceSpec.groovy
@@ -5,18 +5,12 @@ import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Specification
 
-import static MonitorServiceSpec.isWindows
-
-//此处要注意，需要静态引入
 class MonitorServiceSpec extends Specification {
     @Shared
     def monitorService = DefaultMonitorService.INSTANCE
 
-    static def isWindows() {
-        System.getProperty("os.name").startsWith("Windows");
-    }
 
-    @IgnoreIf({ isWindows() })
+    @IgnoreIf({ os.windows })
     def "os level monitor"() {
         given:
         def monitorRequest = new MonitorRequest();
